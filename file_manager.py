4cb4dd0c7ca2f24006669140d0da76065e489962
Muhammaddiyor <armiyatv05@gmail.com>
2025-05-01 17:58:33 +0500

    write function is added

--- file_manager.py
+++ file_manager.py
@@ -9,3 +9,13 @@ def read(filename: str) -> list[dict]:
             reader = csv.DictReader(file)
             return list(reader)
     return []
+
+
+def write(filename: str, data: list[dict]) -> None:
+    if not data:
+        return
+    path = f"data/{filename}"
+    with open(file=path, mode="w", encoding="UTF-8", newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
